c521e8a7a6d4cabe4d3f545827024e15984e147d
Juri Di salvo <Juridisalvo11@gmail.com>
2020-04-17 15:40:25 +0200

    Js PwdGen

---
+++ sheet.js
@@ -0,0 +1,10 @@
+//chiedere nome utente
+var nome_utente = prompt('Nome Utente')
+//chiedere cognome utente
+var cognome_utente = prompt('Cognome Utente - Scrivere il cognome in una sola parola')
+//chiedere colore preferito utente
+var colore_preferito_utente = prompt('Colore Preferito')
+// Anno corrente
+var anno_corrente = '20'
+//generare password utente
+console.log(nome_utente + cognome_utente + colore_preferito_utente + anno_corrente);
